581073232e8ee16eb010faaa8fd02f8e160f768d
kiryagami <138771078+kiryagami@users.noreply.github.com>
2023-07-06 15:19:22 +0530

    Update fashion_main.py

--- fashion_main.py
+++ fashion_main.py
@@ -1,10 +1,4 @@
-Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> # Import necessary libraries
 
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
