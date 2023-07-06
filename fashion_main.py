449c76b1615bcf8fe375a5994acf8789f7fd588b
kiryagami <138771078+kiryagami@users.noreply.github.com>
2023-07-06 15:20:01 +0530

    Update fashion_main.py

--- fashion_main.py
+++ fashion_main.py
@@ -13,8 +13,6 @@ for dirname, _, filenames in os.walk('/kaggle/input'):
 
 # You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
 # You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
-
-pip install --upgrade tensorflow-io
 import numpy as np
 import pandas as pd
 import os
